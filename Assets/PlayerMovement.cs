017880f49c04800ef01718cca08d4c039df584c5
Mehedi Hassan Rico <mhrico2@gmail.com>
2022-01-19 20:02:10 +0600

    feat: Box ran away
    Episode 02 end...

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,17 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Reference to that one rigid body component called rb
     public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.useGravity = false;
+        
     }
 
     // Update is called once per frame
-    void Update()
+    // FixedUpdate becauseu physics LOL
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
     }
 }
