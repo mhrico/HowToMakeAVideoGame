9a25844576ba3ffa0b6def06fe8f9e3f63a51838
Mehedi Hassan Rico <mhrico2@gmail.com>
2022-01-19 19:56:14 +0600

    feat: Rigid body gravity off...

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!!!!");
+        rb.useGravity = false;
     }
 
     // Update is called once per frame
