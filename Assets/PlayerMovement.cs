2f134416247cedb27426be4415bbda6632463b38
Mehedi Hassan Rico <mhrico2@gmail.com>
2022-01-19 20:29:50 +0600

    feat: Control cube left and right

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,17 +4,29 @@ public class PlayerMovement : MonoBehaviour
 {
     // Reference to that one rigid body component called rb
     public Rigidbody rb;
-
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     // FixedUpdate becauseu physics LOL
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
