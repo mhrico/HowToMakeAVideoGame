59803c17affb77c8a2fa931e4a672c8a4cada41c
Mehedi Hassan Rico <mhrico2@gmail.com>
2022-01-22 12:58:58 +0600

    feat: Restart game on game over

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    bool gameHasEnded = false;
     public void endGame()
     {
-        Debug.Log("Game Over!!!");
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over!!!");
+            //TODO: restart
+            restart();
+        }
+    }
+
+    void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
